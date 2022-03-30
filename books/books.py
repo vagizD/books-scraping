260e04b7619d2f039edc1a8dc7336c310b4081a0
Daudov Vagiz <vagiz.0282@gmail.com>
2022-03-30 13:45:10 +0300

    Changing synchronous requests to asynchronous

--- books/books.py
+++ books/books.py
@@ -1,23 +1,59 @@
 import requests
 import json
+import aiohttp
+import asyncio
 
 from books_scraping.pages.books_on_page import BooksOnPage
 from books_scraping.books.books_filename import BooksFilename
 
 
+async def _fetch_page(session, url):
+    async with session.get(url) as response:
+        return response.content
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(_fetch_page(session, url))  # creates coroutine objects
+            grouped_tasks = asyncio.gather(*tasks)
+            return await grouped_tasks
+
+
 def _get_books_content():
     """
-    Extracts books contents from https://books.toscrape.com/catalogue/category/books_1. Iterates over every page of
-    catalogue.
+    Extracts books contents from https://books.toscrape.com/catalogue/category/books_1. Uses asynchronous libraries to
+    reduce the requests completion time.
     :return: list of BookParser objects.
     """
     books_catalogue = []
-    for i in range(1, 51, 1):
-        page_content = requests.get(f'https://books.toscrape.com/catalogue/category/books_1/page-{i}.html').content
+
+    loop = asyncio.get_event_loop()
+    urls = [f'https://books.toscrape.com/catalogue/category/books_1/page-{i}.html' for i in range(1,51,1)]
+    page_contents = loop.run_until_complete(get_multiple_pages(loop, *urls))
+
+    for page_content in page_contents:
         books = BooksOnPage(page_content)
         books_catalogue.append(books.books)
+
     books_catalogue = [item for sublist in books_catalogue for item in sublist]
     return books_catalogue
+
+
+# def _get_books_content():
+#     """
+#     Extracts books contents from https://books.toscrape.com/catalogue/category/books_1. Iterates over every page of
+#     catalogue.
+#     :return: list of BookParser objects.
+#     """
+#     books_catalogue = []
+#     for i in range(1, 51, 1):
+#         page_content = requests.get(f'https://books.toscrape.com/catalogue/category/books_1/page-{i}.html').content
+#         books = BooksOnPage(page_content)
+#         books_catalogue.append(books.books)
+#     books_catalogue = [item for sublist in books_catalogue for item in sublist]
+#     return books_catalogue
 
 
 def _write_books_content(filename: str):
